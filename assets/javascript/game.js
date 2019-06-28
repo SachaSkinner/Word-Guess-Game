8b6075fbe5a87d85d6abdd2d3186b02152dda9c3
Alexandra Skinner <aleksandrayandovka@gmail.com>
2019-06-28 14:19:43 -0500

    wrote some pseudo code, connected all pages and added one video with background picture and some css for it

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+// press any key to start a game - get input
+// write down questions about songs and answers and store them in object
+// find music files and store them with id-s
+// to show a question and a  word hidden with dashes
+// get input from user and if letter matches - open it in word
+// keep going till word is open or guesses left are zero
+// if word was guessed- say woohoo and play this song and show a botton to choose a new song
+// if a word was failed say try one more time and show botton shoose a new song
